f3384d98dd0503b7ced5cb1b572abcf2a5c9085a
hash-bunny <BecquetMae@gmail.com>
2025-11-25 19:09:40 -0600

    idk anymore man im just hoping this is enough to get me like a 40 at this point

--- nodeapi/server.js
+++ nodeapi/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const { Sequelize } = require('sequelize');
+import cors from 'cors';
+import {db} from "./db.js";
+import userRoutes from './routes/user-api.js';
+import loginRoutes from './routes/login-api.js';
+import postRoutes from './routes/post-api.js';
+import commentRoutes from './routes/comment-api.js';
+import likeRoutes from './routes/like-api.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+const corsOptions = {
+    origin: /localhost:\d{4,5}$/i,
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+    methods: 'GET,PUT,POST,DELETE,OPTIONS',
+    maxAge: 1000 * 60 * 60 * 12
+}
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
@@ -15,9 +29,18 @@ sequelize.authenticate()
     .then(() => console.log('Database connected...'))
     .catch(err => console.error('Unable to connect to the database:', err));
 
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use('/api', userRoutes);
+app.use('/api', loginRoutes);
+app.use('/api', postRoutes);
+app.use('/api', commentRoutes);
+app.use('/api', likeRoutes);
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World from Node API!');
+    res.send('Blog site api');
 });
 
 app.listen(PORT, () => {
