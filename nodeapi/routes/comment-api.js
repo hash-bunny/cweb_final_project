49785852800dedeb95abce25edbcb4a92432aee4
hash-bunny <BecquetMae@gmail.com>
2025-11-25 18:14:57 -0600

    Redid the comment-api requests AGAIN

--- nodeapi/routes/comment-api.js
+++ nodeapi/routes/comment-api.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import {authorizeJWT} from "../middleware/auth-middleware";
-import {createComment, getAllCommentsByPostId} from "../controllers/comment-controller";
-
+import {createComment, deleteComment, getAllCommentsByPostId, updateComment} from "../controllers/comment-controller";
 const router = express.Router();
 const apiPath = '/comments';
 
@@ -24,6 +23,32 @@ router.post(apiPath, authorizeJWT, async (req, res) => {
 router.get(`${apiPath}/:postId`, async (req, res) => {
     const result = await getAllCommentsByPostId(req.params.postId);
     if (result?.comments?.length) return res.status(200).json(result);
+})
+
+
+// comment PUT request
+router.put(`${apiPath}/:id`, authorizeJWT, async (req, res) => {
+    if (req.error) {
+        return res.status(401).json({error: req.error});
+    }
+
+    const comment = await updateComment(parseInt(req.params.id), req.body.commentText);
+    if (comment.id) return res.status(204).json(comment);
+
+    return res.status(400).json({error: comment.message, errors: comment.errors});
+})
+
+
+// comment DELETE request
+router.delete(`${apiPath}/:id`, authorizeJWT ,async (req, res) => {
+    if (req.error) {
+        return res.status(401).json({error: req.error});
+    }
+
+    const result = await deleteComment(parseInt(req.params.id));
+    if (result) return res.status(200).json({result: result});
+
+    return res.status(400).json({error: 'Delete failed'});
 })
 
 export default router;
